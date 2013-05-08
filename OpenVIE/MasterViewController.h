63fc3e69c9d0401bce494a0e2568e07c6e710258
wexoo <p.weixlbaumer@gmail.com>
2013-05-09 00:00:17 +0200

    Initial commit
    Created project in Xcode using the Master-Detail template

---
+++ OpenVIE/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  OpenVIE
+//
+//  Created by Philipp Weixlbaumer on 5/8/13.
+//  Copyright (c) 2013 Philipp Weixlbaumer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
